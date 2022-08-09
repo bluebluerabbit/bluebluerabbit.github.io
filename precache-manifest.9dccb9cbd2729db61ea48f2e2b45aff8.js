0df737d2476e7c9257417fdf2f4260ef11b24ab4
Miseon <71369109+bluebluerabbit@users.noreply.github.com>
2022-08-09 21:52:31 +0900

    create monotonememopadpage

---
+++ precache-manifest.9dccb9cbd2729db61ea48f2e2b45aff8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59f6c0ca1269ea49a9e8",
+    "url": "/css/app.3759b332.css"
+  },
+  {
+    "revision": "078749d513b3f22da854",
+    "url": "/css/chunk-vendors.0f1ada3b.css"
+  },
+  {
+    "revision": "b529fcda5a5f88da0af13fa176b78652",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59f6c0ca1269ea49a9e8",
+    "url": "/js/app.ece77dc8.js"
+  },
+  {
+    "revision": "078749d513b3f22da854",
+    "url": "/js/chunk-vendors.8f94aab7.js"
+  },
+  {
+    "revision": "ea501cb10bef945d7c89e4d1ee0c9c32",
+    "url": "/manifest.json"
+  }
+]);
